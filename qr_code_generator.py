24eecbfa1a1e0d6b158f2ed7fa335ac86f3ae9b7
apple <apple@apples-MacBook-Pro-2.local>
2025-03-11 19:38:57 +0530

    simple python program for qr code generator

---
+++ qr_code_generator.py
@@ -0,0 +1,19 @@
+import qrcode
+
+data = "Hello World! This is Kritii"
+
+# Create QR Code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+# Generate Image
+img = qr.make_image(fill="black", back_color="white")
+img.save("mycode.png")
+
+print("QR Code generated successfully!")
